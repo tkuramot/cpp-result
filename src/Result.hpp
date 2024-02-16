9636e0ce9d6fabbd215e3bf1eb2cf2bb4d2c39a7
tkuramot <mabdman512@gmail.com>
2024-02-17 02:39:44 +0900

    orthodox canonical form

--- src/Result.hpp
+++ src/Result.hpp
@@ -10,19 +10,39 @@ class Result {
  public:
   Result(T value, E error, bool has_value)
       : value_(value), error_(error), has_value_(has_value) {}
+
+  Result(const Result &other)
+      : value_(other.value_),
+        error_(other.error_),
+        has_value_(other.has_value_) {}
+
+  Result &operator=(const Result &other) {
+    if (this == &other) {
+      return *this;
+    }
+    value_ = other.value_;
+    error_ = other.error_;
+    has_value_ = other.has_value_;
+  }
+
+  ~Result() {}
+
   T Unwrap() const {
     if (!IsOk()) {
       throw std::runtime_error("Result does not have a value");
     }
     return value_;
   }
+
   E UnwrapErr() const {
     if (!IsErr()) {
       throw std::runtime_error("Result does not have an error");
     }
     return error_;
   }
+
   bool IsOk() const { return has_value_; }
+
   bool IsErr() const { return !has_value_; }
 
  private:
@@ -41,6 +61,18 @@ template <typename T>
 class Value {
  public:
   explicit Value(T value) : value_(value) {}
+
+  Value(const Value &other) : value_(other.value_) {}
+
+  Value &operator=(const Value &other) {
+    if (this == &other) {
+      return *this;
+    }
+    value_ = other.value_;
+  }
+
+  ~Value() {}
+
   template <typename V, typename E>
   operator Result<V, E>() const {
     return Result<V, E>(value_, E(), /* has_value= */ true);
@@ -54,6 +86,18 @@ template <typename E>
 class Error {
  public:
   explicit Error(E error) : error_(error) {}
+
+  Error(const Error &other) : error_(other.error_) {}
+
+  Error &operator=(const Error &other) {
+    if (this == &other) {
+      return *this;
+    }
+    error_ = other.error_;
+  }
+
+  ~Error() {}
+
   template <typename V, typename F>
   operator Result<V, F>() const {
     return Result<V, F>(V(), error_, /* has_value= */ false);
